026e2facd1ad4b02c36fbd935c78c2ad8e1f55fe
John Zacarias Jekel <JZJisawesome123@gmail.com>
2023-01-08 10:39:22 -0500

    Add some starting files

---
+++ src/legv8emulator.rs
@@ -0,0 +1,37 @@
+/* NAME//TODO
+ * By: John Jekel
+ *
+ * TODO description
+ *
+*/
+
+/* Imports */
+
+//TODO (include "use" and "mod" here)
+
+/* Constants */
+
+//TODO
+
+/* Macros */
+
+//TODO (also pub(crate) use the_macro statements here too)
+
+/* Static Variables */
+
+//TODO
+
+/* Types */
+
+//TODO
+
+/* Associated Functions and Methods */
+
+//TODO
+
+/* Functions */
+
+fn main() {
+    //TODO use HashMap for sparse memory since we don't care about performance
+    println!("Hello, world!");
+}
